98d5fa0b85bb415c98a6d31f02122c8d0ff57687
Logan <loganreadinger1@gmail.com>
2020-10-12 02:35:38 -0400

    Revert "Renamed TestBiome to MooShroomMountains"
    This reverts commit aac454aa96507938ddc47f25bf73559f6d998a56.

--- src/main/java/com/sacredninja/sacredsbiomes/world/biomes/TestBiome.java
+++ src/main/java/com/sacredninja/sacredsbiomes/world/biomes/TestBiome.java
@@ -15,19 +15,30 @@ import net.minecraft.world.gen.placement.FrequencyConfig;
 import net.minecraft.world.gen.placement.HeightWithChanceConfig;
 import net.minecraft.world.gen.placement.Placement;
 
-public class MooShroomMountains extends Biome{
-	public MooShroomMountains(Builder biomeBuilder) {
+public class TestBiome extends Biome{
+
+	public TestBiome(Builder biomeBuilder) {
 		super(biomeBuilder);
 		
 		this.addSpawn(
 				EntityClassification.CREATURE,
-				new SpawnListEntry(EntityType.MOOSHROOM, 50, 3, 6));
+				new SpawnListEntry(EntityType.BEE, 20, 2, 5));
 		
 		this.addCarver(
 				GenerationStage.Carving.AIR,
 				Biome.createCarver(
 						WorldCarver.CAVE,
-						new ProbabilityConfig(0.14285710f))); // Default : 0.14285715f
+						new ProbabilityConfig(0.14285715f)));
+		
+		this.addCarver(
+				GenerationStage.Carving.AIR,
+				Biome.createCarver(
+						WorldCarver.HELL_CAVE,
+						new ProbabilityConfig(0.02f)));
+		
+		this.addFeature(
+				GenerationStage.Decoration.UNDERGROUND_DECORATION,
+				Feature.FOSSIL.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG).withPlacement(Placement.CHANCE_PASSTHROUGH.configure(new ChanceConfig(128))));
 		
 		this.addFeature(
 				GenerationStage.Decoration.VEGETAL_DECORATION,
@@ -38,26 +49,27 @@ public class MooShroomMountains extends Biome{
 								.withConfiguration(DefaultBiomeFeatures.BIG_BROWN_MUSHROOM)))
 				.withPlacement(
 						Placement.COUNT_HEIGHTMAP
-						.configure(new FrequencyConfig(5))));
+						.configure(new FrequencyConfig(1))));
 	    
 		this.addFeature(
 				GenerationStage.Decoration.VEGETAL_DECORATION,
 				Feature.RANDOM_PATCH.withConfiguration(
 						DefaultBiomeFeatures.BROWN_MUSHROOM_CONFIG)
 				.withPlacement(Placement.COUNT_CHANCE_HEIGHTMAP
-						.configure(new HeightWithChanceConfig(5, 0.25F))));
+						.configure(new HeightWithChanceConfig(1, 0.25F))));
 	    
 		this.addFeature(
 	    		GenerationStage.Decoration.VEGETAL_DECORATION,
 	    		Feature.RANDOM_PATCH.withConfiguration(
 	    				DefaultBiomeFeatures.RED_MUSHROOM_CONFIG)
 	    		.withPlacement(Placement.COUNT_CHANCE_HEIGHTMAP_DOUBLE
-	    				.configure(new HeightWithChanceConfig(5, 0.125F))));
+	    				.configure(new HeightWithChanceConfig(1, 0.125F))));
 	   
-		DefaultBiomeFeatures.addLakes(this);
 		DefaultBiomeFeatures.addOres(this);
 		DefaultBiomeFeatures.addExtraGoldOre(this);
 		DefaultBiomeFeatures.addExtraEmeraldOre(this);
+		
+		
 	}
 
 	
