19f484c2b4baf10845a28957aa6f2f85f52120c8
Logan <loganreadinger1@gmail.com>
2020-10-11 22:10:50 -0400

    Update SacredsBiomes.java
    Added the modEventBus and custom method to register biomes.

--- src/main/java/com/sacredninja/sacredsbiomes/SacredsBiomes.java
+++ src/main/java/com/sacredninja/sacredsbiomes/SacredsBiomes.java
@@ -1,45 +1,57 @@
 package com.sacredninja.sacredsbiomes;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
+import com.sacredninja.sacredsbiomes.init.BiomeInit;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("sacredsbiomes")
+@Mod.EventBusSubscriber(modid = SacredsBiomes.MOD_ID, bus = Bus.MOD)
 public class SacredsBiomes{
-    private static final Logger LOGGER = LogManager.getLogger();
-
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "sacredsbiomes";
+    public static SacredsBiomes instance;
+    
     public SacredsBiomes() {
+    	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        modEventBus.addListener(this::doClientStuff);
+
+    	
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
+        
+        BiomeInit.BIOMES.register(modEventBus);
+        
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event){
-    	
+    @SubscribeEvent
+    public static void onRegisterBiomes(final RegistryEvent.Register<Biome> event) {
+    	BiomeInit.registerBiomes();
+    }
+    
+    private void setup(final FMLCommonSetupEvent event){	
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
-  
     }
 
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-  
     }
 }
